c7795940d46024c8322a89342e38c75da44735aa
Ellen Littow <143400929+EllenLitt@users.noreply.github.com>
2024-04-01 14:33:04 +0300

    Update CalendarScreen.js

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,18 +1,51 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
 
-export default function CalendarScreen(){
-    return (
-        <View style={styles.container}>
-            <Text style={{fontSize: 20}} >Calendar</Text>
-        </View>
-    );
-}
+LocaleConfig.locales['en'] = {
+  monthNames: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  dayNames: [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  dayNamesShort: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+};
+LocaleConfig.defaultLocale = 'en';
+
+const CalendarScreen = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); // Muunnetaan päivämäärä merkkijonoksi
+
+  return (
+    <View style={styles.container}>
+      <Calendar
+        current={selectedDate}
+        onDayPress={(day) => setSelectedDate(day.dateString)} // Päivän merkkijonon asettaminen
+        monthFormat={'MMMM yyyy'}
+        firstDay={0}
+        enableSwipeMonths={true}
+        locale={'en'}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 20,
+  },
 });
+
+export default CalendarScreen;
