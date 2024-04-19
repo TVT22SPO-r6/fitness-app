83407c63b858a98421b39f3897997ca1e1d2791e
Ellen Littow <143400929+EllenLitt@users.noreply.github.com>
2024-04-19 10:13:39 +0300

    added marked dates

--- components/Day.js
+++ components/Day.js
@@ -1,81 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Event = ({ event }) => {
-    const eventTime = new Date(event.eventDate).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  
-    return (
-      <View style={[styles.event, { top: event.top, height: event.height }]}>
-        <Text style={{ color: 'white' }}>{eventTime}  {event.description}</Text>
-      </View>
-    );
-  };
-  
-  
 const Day = ({ selectedDate, events }) => {
-  
-  const selectedDateEvents = events.filter(event => {
-    const eventDate = new Date(event.eventDate);
-    const eventDateString = eventDate.toISOString().split('T')[0];
-    return eventDateString === selectedDate;
-  });
-
-
-  const sortedEvents = selectedDateEvents.sort((a, b) => new Date(a.eventDate) - new Date(b.eventDate));
-
-
-  const groupEventsByTime = (events) => {
-    const groupedEvents = [];
-    let currentGroup = [];
-    let currentEndTime = null;
-  
-    events.forEach((event) => {
-      const eventTime = new Date(event.eventDate);
-      
-      if (!currentEndTime || eventTime <= currentEndTime) {
-   
-        currentGroup.push(event);
-        currentEndTime = eventTime;
-      } else {
-      
-        groupedEvents.push(currentGroup);
-        currentGroup = [event];
-        currentEndTime = eventTime;
-      }
-    });
-    
-    if (currentGroup.length > 0) {
-      groupedEvents.push(currentGroup);
-    }
-    
-    return groupedEvents;
-  };
-
-  const renderEvents = () => {
-    
-    const groupedEvents = groupEventsByTime(sortedEvents);
-
-   
-    return groupedEvents.map((group, groupIndex) => (
-      <View key={groupIndex} style={styles.eventGroup}>
-        {group.map((event, eventIndex) => (
-          <Event key={eventIndex} event={event} />
-        ))}
-      </View>
-    ));
-  };
-
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('en-GB', options);
-  };
+  const renderEvents = () => events.map((event, index) => (
+    <View key={index} style={styles.event}>
+      <Text style={styles.eventText}>
+        Time: {new Date(event.eventDateTime).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+        {'\n'}Description: {event.description}
+      </Text>
+    </View>
+  ));
 
   return (
     <View style={styles.container}>
-      <Text style={styles.date}>{formatDate(selectedDate)}</Text>
+      <Text style={styles.date}>{new Date(selectedDate).toLocaleDateString('en-GB', {
+        weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'
+      })}</Text>
       <View style={styles.eventsContainer}>
-        {renderEvents()}
+        {events.length > 0 ? renderEvents() : <Text>No Events</Text>}
       </View>
     </View>
   );
@@ -93,20 +35,19 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   eventsContainer: {
-    position: 'relative',
     width: '100%',
-  },
-  eventGroup: {
-    flexDirection: 'column', 
   },
   event: {
     backgroundColor: '#1E90FF',
-    padding: 5,
+    padding: 10,
     borderRadius: 5,
     width: '90%',
+    marginVertical: 5,
     marginLeft: '5%',
-    marginBottom: 5,
   },
+  eventText: {
+    color: 'white',
+  }
 });
 
 export default Day;
