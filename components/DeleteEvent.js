bdf2edd613e8f0708a54c4c3ac13ab45d55b0db5
Ellen Littow <143400929+EllenLitt@users.noreply.github.com>
2024-04-22 10:43:32 +0300

    Update DeleteEvent.js

--- components/DeleteEvent.js
+++ components/DeleteEvent.js
@@ -1,13 +1,9 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
 const deleteEvent = async (event) => {
     try {
         const eventsString = await AsyncStorage.getItem("@events");
         if (eventsString !== null) {
             const events = JSON.parse(eventsString);
-            const updatedEvents = events.filter(
-                (event) => event.eventDateTime !== event.eventDateTime
-            );
+            const updatedEvents = events.filter((e) => e.eventDateTime !== event.eventDateTime);
             await AsyncStorage.setItem("@events", JSON.stringify(updatedEvents));
             console.log("Event removed successfully:", event);
         }
